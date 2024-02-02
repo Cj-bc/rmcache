67de6cfeaab825194960022a460453829dcb1ecc
Cj-bc <cj.bc-sd@outlook.jp>
2024-02-03 02:36:37 +0900

    Fix: flake> renamed project name

--- flake.nix
+++ flake.nix
@@ -31,8 +31,8 @@
           pkgs = nixpkgsFor.${system};
         in
         {
-          go-hello = pkgs.buildGoModule {
-            pname = "go-hello";
+          rmcache = pkgs.buildGoModule {
+            pname = "rmcache";
             inherit version;
             # In 'nix develop', we don't need a copy of the source tree
             # in the Nix store.
@@ -66,6 +66,6 @@
       # The default package for 'nix build'. This makes sense if the
       # flake provides only one package or there is a clear "main"
       # package.
-      defaultPackage = forAllSystems (system: self.packages.${system}.go-hello);
+      defaultPackage = forAllSystems (system: self.packages.${system}.rmcache);
     };
 }

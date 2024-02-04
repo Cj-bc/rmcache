07b71f5a42a33eb6b3198566b04d9deef36cc45f
Cj-bc <cj.bc-sd@outlook.jp>
2024-02-05 01:11:49 +0900

    Change: main> scratching code

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"log/slog"
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
+type Config struct {
+	Programs map[string]program
+}
+
+type program struct {
+	Run   []string
+	Paths []string
+}
+
+func which(progname string) bool {
+	return false
+}
 
 func main() {
-	fmt.Println("Hello Nix!")
+	logger := slog.Default()
+
+	var cfg *Config
+	_, err := toml.DecodeFile("example.toml", &cfg)
+	if err != nil {
+		logger.Error("Failed to decode file", "error", err)
+		os.Exit(-1)
+	}
+
+	for name, program := range cfg.Programs {
+		if !which(name) {
+			logger.Info("Program isn't found. Skipping entry", "program", name)
+			continue
+		}
+
+		if len(program.Run) != 0 {
+			// Run it
+		}
+
+	}
 }
